888c8660b8db7f05926dc5b430c2829485f44558
karina-krupodior <karinakrupodior@gmail.com>
2025-05-27 10:39:22 -0600

    added BreadType enum

---
+++ DELI-cious/src/main/java/models/enums/BreadType.java
@@ -0,0 +1,17 @@
+package models.enums;
+//When they order, they choose the type of bread that they
+//would like (white, wheat, rye, or wrap).
+
+public enum BreadType {
+    WHITE("White"), WHEAT("Wheat"), RYE("Rye"), WRAP("Wrap");
+
+    private final String displayName;
+
+    BreadType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
