716bd155f6a1145b6bec24c094b560fc1b68e2a5
karina-krupodior <karinakrupodior@gmail.com>
2025-05-28 10:58:56 -0600

    added Sandwich

--- DELI-cious/src/main/java/models/Sandwich.java
+++ DELI-cious/src/main/java/models/Sandwich.java
@@ -1,13 +1,19 @@
 package models;
+
+import models.enums.BreadType;
+import models.enums.Size;
+
 //The sandwich is the core of our business.
 public class Sandwich implements MenuItem {
     private String name;
     private double price;
+    private Size size;
+    private BreadType bread;
+    private  boolean toasted;
 
     public Sandwich(String name, double price) {
         this.name = name;
         this.price = price;
-
     }
 
     @Override
--- DELI-cious/src/main/java/services/SandwichBuilder.java
+++ DELI-cious/src/main/java/services/SandwichBuilder.java
@@ -1,7 +1,25 @@
 package services;
 
+import models.enums.BreadType;
 import models.enums.Size;
 
 public class SandwichBuilder {
     private Size size;
+    private BreadType bread;
+    private  boolean toasted;
+
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public void setBread (BreadType bread) {
+        this.bread = bread;
+
+    }
+
+    public void setToasted(boolean toasted) {
+        this.toasted = toasted;
+    }
+
 }
